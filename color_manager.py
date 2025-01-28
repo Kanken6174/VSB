56a1913a16f829509f0aa3eb3998a60c645cf4c3
Kanken6174 <onyx63670@gmail.com>
2025-01-28 17:35:27 +0100

    commit before added features

--- color_manager.py
+++ color_manager.py
@@ -1,11 +1,14 @@
-#color_manager.py
+# color_manager.py
 class ColorManager:
     def __init__(self):
         self.colors = [
-            "#1f77b4","#ff7f0e","#2ca02c","#d62728","#9467bd",
-            "#8c564b","#e377c2","#7f7f7f","#bcbd22","#17becf"
+            "#1f77b4", "#ff7f0e", "#2ca02c", "#9467bd",
+            "#8c564b", "#7f7f7f", "#bcbd22", "#17becf",
+            "#aec7e8", "#98df8a", "#c5b0d5", "#c49c94",
+            "#9edae5", "#ffbb78", "#c7c7c7", "#393b79"
         ]
         self.index = 0
+
     def get_next_color(self):
         c = self.colors[self.index % len(self.colors)]
         self.index += 1
